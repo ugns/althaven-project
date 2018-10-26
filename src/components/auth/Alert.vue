42139b8a736c9ef76738557a6e57b0120a4c1d04
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2018-10-26 01:31:06 -0400

    Make alert auto dismissing

--- src/components/auth/Alert.vue
+++ src/components/auth/Alert.vue
@@ -1,6 +1,10 @@
 <template>
-  <b-alert
-    :show="hasAuthenticationStatus"
+  <b-alert class="text-center"
+    :show="hasAuthenticationStatus && dismissCountDown"
+    dismissible
+    fade
+    @dismissed="dismissCountDown=0"
+    @dismiss-count-down="countDownChanged"
     :variant="authenticationStatus.variant">{{ authenticationStatus.message }}.</b-alert>
 </template>
 
@@ -8,6 +12,24 @@
 import { mapGetters } from 'vuex';
 
 export default {
+  data () {
+    return {
+      dismissSecs: 5,
+      dismissCountDown: 0
+    }
+  },
+  methods: {
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    }
+  },
+  watch: {
+    hasAuthenticationStatus: function (val, oldVal) {
+      if (val && !oldVal) {
+        this.dismissCountDown = this.dismissSecs
+      }
+    }
+  },
   mounted() {
     this.$store.dispatch("auth/clearAuthenticationStatus")
   },
