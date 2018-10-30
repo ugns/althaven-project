21e4b99fa9412e5b5abdda1289cf440e14f8d6bd
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2018-10-30 01:45:21 -0400

    Make chat module component dynamic

--- src/views/Webchat.vue
+++ src/views/Webchat.vue
@@ -1,24 +1,45 @@
 <template>
-  <TheLounge
-    :username="user.nickname">
-  </TheLounge>
+  <div>
+    <b-button-group>
+      <b-button
+        v-for="module in modules"
+        :key="module"
+        :class="[{ active: currentChat === module }]"
+        variant="primary"
+        @click="currentChat = module">{{ module }}</b-button>
+    </b-button-group>
+    <component :is="currentChatComponent" :username="user.nickname" class="module"/>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
+import { mapGetters, mapState } from 'vuex'
 import TheLounge from '@/components/chat/TheLounge.vue'
+// import LightIRC from '@/components/chat/LightIRC.vue'
+import Kiwi from '@/components/chat/Kiwi.vue'
 
 export default {
+  data () {
+    return {
+      currentChat: 'TheLounge',
+      modules: ['TheLounge', 'Kiwi']
+    }
+  },
   computed: {
     ...mapState('auth', {
         user: state => state.userInfo
     }),
     ...mapGetters('auth', {
       isAuthenticated: 'isAuthenticated',
-    })
+    }),
+    currentChatComponent: function () {
+      return this.currentChat
+    }
   },
   components: {
-    TheLounge
+    TheLounge,
+    // LightIRC,
+    Kiwi
   }
 }
 </script>
