c5a2ea2882a7a14095cf719fb340115ee95c9608
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2018-10-30 00:44:38 -0400

    Add initial Kiwi IRC Client component

---
+++ src/components/chat/Kiwi.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mx-auto w-75 py-2">
+    <div>
+      <b-embed
+        type="iframe"
+        aspect="16by9"
+        allowfullscreen
+        :src="baseUrl">
+      </b-embed>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Kiwi',
+  data () {
+    return {
+      baseUrl: process.env.VUE_APP_KIWI_URL || "https://kiwiirc.com/nextclient/"
+    }
+  }
+}
+</script>
